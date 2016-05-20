6d8cfc2be75cd966de6ee2c0734cd54e0f7c9694
Dmitriy Popelnitskiy <popelnitskiyd@arkadium.com>
2016-05-20 14:30:00 +0300

    task: made some changes

--- index.js
+++ index.js
@@ -6,5 +6,4 @@ module.exports = {
 	ShowTransitionAlphaAndMovement: require('./src/transition/show/ShowTransitionAlphaAndMovement'),
 	ShowTransitionAlpha: require('./src/transition/show/ShowTransitionAlpha'),
 	HideTransitionMovement: require('./src/transition/hide/HideTransitionMovement')
-
 };
